cc6d5a8c55e0f8ce51334064bb3a467a4c83b282
Karoline Spohn <118634774+karolinespohn@users.noreply.github.com>
2024-05-03 12:39:31 +0200

    first stab at lexer :)

---
+++ src/main/kotlin/Lexer.kt
@@ -0,0 +1,45 @@
+import java.lang.IllegalArgumentException
+
+sealed class Token{
+    data class Number(val value : Int) : Token()
+    object Plus : Token()
+    object Minus : Token()
+    object Times : Token()
+    object OpenParen : Token()
+    object ClosedParen : Token()
+    object Element : Token()
+    object EOF : Token()
+}
+
+class Lexer(val text : String) {
+    var pointer = 0
+
+    fun scanText() : List<Token> {
+        val tokens = mutableListOf<Token>()
+
+        while (pointer < text.length) {
+            tokens.add(chooseToken())
+        }
+        tokens.add(Token.EOF)
+        return tokens
+    }
+
+    private fun chooseToken() : Token =
+        when (text[pointer]) {
+            '+' -> Token.Plus.also { pointer++ }
+            '-' -> Token.Minus.also { pointer++ }
+            '*' -> Token.Times.also { pointer++ }
+            '(' -> Token.OpenParen.also { pointer++ }
+            ')' -> Token.ClosedParen.also { pointer++ }
+            'e' -> Token.Element.also { pointer += 7 }
+            else   -> {
+                val match = "[0-9]*".toRegex().matchAt(text, pointer)
+                    ?: throw IllegalArgumentException()
+
+                pointer += match.value.length
+                Token.Number(match.value.toInt())
+            }
+        }
+
+}
+
